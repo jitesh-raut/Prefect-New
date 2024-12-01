53c8bbdd48d57b2b36197580c0ca9c92628756f1
jitesh-raut <jiteshraut@outlook.com>
2024-12-01 18:01:59 +0530

    deleted files

--- blob_download_test.py
+++ blob_download_test.py
@@ -5,9 +5,7 @@ from prefect_azure.blob_storage import blob_storage_download
 @flow
 async def example_blob_storage_download_flow():
     connection_string = (
-        # "DefaultEndpointsProtocol=https;AccountName=storageprefectexp;"
-        # "AccountKey=igKnSDj4w6l67t7yi+V7ngpFdzOY4lCPokBZiYmpvHyBB7yrv1BBnupO+1LXU59syEZmc43NyxvX+AStQh1l6Q==;"
-        # "EndpointSuffix=core.windows.net"
+        # "****"
     )
     blob_storage_credentials = AzureBlobStorageCredentials(
         connection_string=connection_string,
